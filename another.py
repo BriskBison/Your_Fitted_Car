d3f7e6928dbdba731a18958c2b978e5d1f16bfb8
BriskBison <114991041+BriskBison@users.noreply.github.com>
2025-01-26 20:18:51 +0100

    Update another.py
    moved imports at the top, added "prints" before Table() and Chart()

--- another.py
+++ another.py
@@ -1,37 +1,27 @@
 import pandas
 import csv
+import matplotlib.pyplot as plt
+import numpy as np
 
 class Table():
     def other(self,df):
         self.df = df
-
-        print("Let's how other people have chosen")
         df = pandas.read_csv("cars.csv")
         print(df)
 
+print("Let's see how others chose"))
 Table()
-
-import matplotlib.pyplot as plt
-import numpy as np
 
 class Chart():
     def view(self,y,mylabels):
         self.y = y
         self.mylabels = mylabels
+        y = np.array([47, 38, 15])
+        mylabels = ["Dodge RAM", "Aston Martin DB12", "Toyota Supra"]
+        plt.pie(y, labels = mylabels)
+        plt.legend()
+        plt.pie(y)
+        plt.show()
 
-    y = np.array([47, 38, 15])
-
-    mylabels = ["Dodge RAM", "Aston Martin DB12", "Toyota Supra"]
-
-    plt.pie(y, labels = mylabels)
-    plt.legend()
-    plt.pie(y)
-    plt.show()
-
+print("Let's see a chart with people choices")
 Chart()
-
-
-# """ TODO 5  Enumerate, enum, dekoratory sortowanie, lambdę  oraz generatory można użyć w
-#       trakcie pisania informacji technicznych na temat samochodu, który został wybrany """
-# """ TODO 6 Wszystkie kolejne rzeczy których się dowiem mogą być użyte przy porównywaniu
-#            samochodów i ich osiągów""
